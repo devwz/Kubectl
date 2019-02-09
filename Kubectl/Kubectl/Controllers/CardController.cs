377e074e5e6d99cb2c3c43add6ef1ef3bbca6322
DevWz <wellington.zaneze@outlook.com.br>
2019-02-09 14:11:22 -0200

    Add CardController.

---
+++ Kubectl/Kubectl/Controllers/CardController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kubectl.Core;
+using Kubectl.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Kubectl.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardController : Controller
+    {
+        ApplicationDbContext context;
+
+        public CardController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult<IEnumerable<Card>> Get()
+        {
+            return context.CardContext.ToList();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public ActionResult<Card> Get(int id)
+        {
+            Card card = context.CardContext.Find(id);
+
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return card;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public ActionResult<Card> Post(Card card)
+        {
+            context.CardContext.Add(card);
+            context.SaveChanges();
+
+            return CreatedAtAction("Get", new { id = card.Id }, card);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public ActionResult<Card> Put(int id, Card card)
+        {
+            if (id != card.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(card).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public ActionResult<Card> Delete(int id)
+        {
+            Card card = context.CardContext.Find(id);
+
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            context.CardContext.Remove(card);
+            context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
